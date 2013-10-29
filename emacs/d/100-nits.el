5b6f59e9a8c2ef9697900edac5fc0932393a70bb
Mark Plaksin <happy@mcplaksin.org>
2013-10-29 14:40:34 -0400

    More nits including initial helm setup.

--- emacs/d/100-nits.el
+++ emacs/d/100-nits.el
@@ -1,3 +1,11 @@
 (menu-bar-mode -1)
 (tool-bar-mode -1)
 (blink-cursor-mode -1)
+(put 'upcase-region 'disabled nil)
+(setq term-debug nil)
+
+;; helm
+(global-set-key (kbd "C-^") 'helm-happy)
+(setq helm-idle-delay 0.1)
+(setq helm-input-idle-delay .1)
+
