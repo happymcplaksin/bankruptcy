fe15f929cda01edebc3ce29ebdb9b1ca412ae883
Mark Plaksin <happy@mcplaksin.org>
2013-10-30 13:31:51 -0400

    play

--- emacs/init.el
+++ emacs/init.el
@@ -19,3 +19,17 @@
 	(setq files (cdr files))))))
 (map-load-libraries-in-order (concat map-emacs-etc "/d"))
 
+(custom-set-variables
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(erc-nick "happymcplaksin"))
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ )
+
+yabba
