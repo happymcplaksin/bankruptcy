1dce24ccc93c532b78a3e31e2e90b636e9ae6fe2
Mark Plaksin <happy@mcplaksin.org>
2013-11-01 08:23:52 -0400

    gnus: Protect me from C-w in Group buffer

--- emacs/d/600-gnus.el
+++ emacs/d/600-gnus.el
@@ -1,1 +1,7 @@
 (setq gnus-select-method '(nntp "news.gmane.org"))
+
+;; C-w in Group buffer always causes me grief
+(gnus-define-keys gnus-group-mode-map "\C-w"
+		  (lambda ()
+		    (interactive)
+		    (message "Saved your own butt, didn't you?")))
