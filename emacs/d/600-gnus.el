21ac4c23b82a6a61b2f444d3f7544d1c47c272ff
Mark Plaksin <happy@mcplaksin.org>
2013-11-01 09:09:31 -0400

    Gnus nits.

--- emacs/d/600-gnus.el
+++ emacs/d/600-gnus.el
@@ -7,3 +7,8 @@
 		    (message "Saved your own butt, didn't you?")))
 
 (define-key gnus-group-mode-map "z" 'helm-happy)
+
+(setq
+ gnus-list-groups-with-ticked-articles nil
+ gnus-topic-display-empty-topics nil
+ gnus-topic-indent-level 4)
