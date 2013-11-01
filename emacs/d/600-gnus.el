1bbb64e2421000c21d96a0f170a21af190ae0ef3
Mark Plaksin <happy@mcplaksin.org>
2013-11-01 11:18:04 -0400

    BBDB config.

--- emacs/d/600-gnus.el
+++ emacs/d/600-gnus.el
@@ -15,3 +15,18 @@
  gnus-topic-indent-level 4)
 
 (add-hook 'message-mode-hook 'flyspell-mode)
+
+(setq user-mail-address "happy@usg.edu")
+
+(require 'bbdb)
+(bbdb-initialize 'gnus 'message)
+(setq bbdb-default-area-code 706)
+(setq bbdb/mail-auto-create-p nil)
+(setq bbdb-notice-auto-save-file t)
+(setq bbdb-use-pop-up nil)
+(setq bbdb-completion-display-record nil)
+(setq bbdb-complete-name-allow-cycling t)
+(setq bbdb-dwim-net-address-allow-redundancy t)
+(setq bbdb-quiet-about-name-mismatches t)
+
+(add-hook 'mail-setup-hook 'bbdb-mail-aliases)
