dddc75d9ead10ab2cc047abb05bcbf74a09715ed
Mark Plaksin <happy@mcplaksin.org>
2013-11-01 10:35:15 -0400

    Start Gnus plus flyspell

--- emacs/d/600-gnus.el
+++ emacs/d/600-gnus.el
@@ -1,4 +1,5 @@
 (setq gnus-select-method '(nntp "news.gmane.org"))
+(gnus)
 
 ;; C-w in Group buffer always causes me grief
 (gnus-define-keys gnus-group-mode-map "\C-w"
@@ -12,3 +13,5 @@
  gnus-list-groups-with-ticked-articles nil
  gnus-topic-display-empty-topics nil
  gnus-topic-indent-level 4)
+
+(add-hook 'message-mode-hook 'flyspell-mode)
