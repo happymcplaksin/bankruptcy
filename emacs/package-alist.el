f68f243eb46b38564c9f663968f19efa95416d08
Mark Plaksin <happy@mcplaksin.org>
2013-11-01 09:13:22 -0400

    package-alist.el - Poor man's version control.

---
+++ emacs/package-alist.el
@@ -0,0 +1,16 @@
+;; Poor man's version control for MELPA packages.
+;; Once in a while I save the value of package-alist.
+(setq package-alist
+      ((w3m .
+	    [(20131021 2047)
+	     nil "an Emacs interface to w3m [cvs]"])
+       (puppet-mode .
+	    [(20130819 1946)
+	     nil "Major mode for Puppet manifests [github]"])
+       (powershell-mode .
+            [(20100203 458)
+	     nil "Mode for editing Powershell scripts [svn]"])
+       (helm .
+	    [(20131031 1743)
+	     nil "Helm is an Emacs incremental and narrowing framework [github]"])))
+
