402a4441412fd67abec12d971f5aaa86f789fc82
Mark Plaksin <happy@usg.edu>
2015-04-29 08:59:00 -0400

    Fix helm-happy for newer helm

--- emacs/d/800-helm.el
+++ emacs/d/800-helm.el
@@ -6,14 +6,20 @@
 (setq helm-idle-delay 0.05)
 (setq helm-input-idle-delay .05)
 
+(setq helm-happy-sources '(
+                         helm-source-noodle
+			 helm-source-buffers-list
+                         helm-source-multi-term
+			 helm-source-recentf))
+
 (defun helm-happy ()
   "helm-happy, duped helm-mini plus changes"
   (interactive)
   (require 'helm-files)
+  (unless helm-source-buffers-list
+    (setq helm-source-buffers-list
+          (helm-make-source "Buffers" 'helm-source-buffers)))
   (let ((helm-ff-transformer-show-only-basename nil))
-    (helm-other-buffer '(
-                         helm-source-noodle
-			 helm-source-buffers-list
-                         helm-source-multi-term
-			 helm-source-recentf)
-                       "*helm happy*")))
+    (helm :sources helm-happy-sources
+          :buffer "*helm happy*"
+          :truncate-lines t)))
